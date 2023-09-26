3a9bbe6e4d6911b9def9488f03bd9cf3ad60e807
punit <punitvishwakarma47@gmail.com>
2023-09-26 21:28:17 +0530

    final project

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 300
+}); 
+
+ScrollReveal().reveal('.mynav');
+
+//Heaing strat
+ScrollReveal().reveal('.heading', { origin:'top' });
+
+// section One strat  To-img 
+ScrollReveal().reveal('.to-img', { origin:'left' });
+ScrollReveal().reveal('#img-to', { origin:'right' });
+
+// section 2 strats 
+ScrollReveal().reveal('.text-bg', { origin:'top' });
+ScrollReveal().reveal('.team-text', { origin:'buttom' });
+
+// section 3 strats gallery 
+ScrollReveal().reveal('.gallery-img', { origin:'left' });
+ScrollReveal().reveal('.gallery-second-img', { origin:'right' });
+
+// section 4 strats pricing-text 
+ScrollReveal().reveal('.pricing-main', { origin:'top' });
+ScrollReveal().reveal('.pricing-text', { origin:'left' });
+
+// section 5 About strat
+ScrollReveal().reveal('.about-main', { origin:'right' });
+
+// section 6 from blog strats 
+ScrollReveal().reveal('.img-box', { origin:'left' });
+
+// section 8 Vide ka icon  strat
+ScrollReveal().reveal('.icon', { origin:'left' });
+
+// footer  strat 
+ScrollReveal().reveal('.footer-main', { origin:'right' });
+
+
